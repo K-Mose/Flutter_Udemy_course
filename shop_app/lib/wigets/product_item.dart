5b3fb25ca92376f133bbddb66ba7bf554fe77a92
Mose <km_413@naver.com>
2023-04-27 16:42:39 +0900

    Using Consumer instead of Provider.of

--- shop_app/lib/wigets/product_item.dart
+++ shop_app/lib/wigets/product_item.dart
@@ -17,45 +17,52 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
-
+    /*
+    리스너 방식
+    Provider.of -> 전체 위젯 rebuild
+    Consumer -> 하위 위젯을 consumer로 생성, 해당 부분만 rebuild
+    */
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        footer: GridTileBar(
-          backgroundColor: Colors.black54,
-          leading: IconButton(
-            icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            onPressed: () => {product.changeFavoriteStatus()},
-            color: Theme.of(context).accentColor,
-          ),
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-          trailing: IconButton(
-            icon: const Icon(Icons.shopping_cart),
-            onPressed: () => {},
-            color: Theme.of(context).accentColor,
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          footer: GridTileBar(
+            backgroundColor: Colors.black54,
+            leading: Consumer<Product>(
+              // context / product(provider 객체) / child (rebuild 될 객체)
+              builder: (_, product, child) => IconButton(
+                icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
+                onPressed: () => {product.changeFavoriteStatus()},
+                color: Theme.of(context).accentColor,
+              ),
             ),
-        ),
-        child: GestureDetector(
-          onTap: () {
-            // 현재 스크린에 없는 상태를 가져올 수 없음. 
-            // Navigator.of(context).push(MaterialPageRoute(
-            //   builder: (ctx) => ProductDetailScreen(title, price)
-            // ));
-            Navigator.of(context).pushNamed(
-              ProductDetailScreen.routeName, 
-              arguments: product.id,
-            );
-          },
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+              icon: const Icon(Icons.shopping_cart),
+              onPressed: () => {},
+              color: Theme.of(context).accentColor,
+              ),
+          ),
+          child: GestureDetector(
+            onTap: () {
+              // 현재 스크린에 없는 상태를 가져올 수 없음. 
+              // Navigator.of(context).push(MaterialPageRoute(
+              //   builder: (ctx) => ProductDetailScreen(title, price)
+              // ));
+              Navigator.of(context).pushNamed(
+                ProductDetailScreen.routeName, 
+                arguments: product.id,
+              );
+            },
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            ),
           ),
         ),
-      ),
     );
   }
 
