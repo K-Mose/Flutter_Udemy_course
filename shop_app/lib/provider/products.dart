1ccbb9cf687cdb9ba26f0d64eb11454554305676
Mose <km_413@naver.com>
2023-04-28 11:19:28 +0900

    Add More Products

--- shop_app/lib/provider/products.dart
+++ shop_app/lib/provider/products.dart
@@ -40,6 +40,76 @@ class Products with ChangeNotifier {
       imageUrl:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
     ),
+    Product(
+      id: 'p5',
+      title: 'Blue Sweater',
+      description: 'Stay cozy with this soft blue sweater.',
+      price: 39.99,
+      imageUrl: '',
+    ),
+    Product(
+      id: 'p6',
+      title: 'Leather Jacket',
+      description: 'A sleek and stylish leather jacket for any occasion.',
+      price: 99.99,
+      imageUrl: ''
+    ),
+    Product(
+      id: 'p7',
+      title: 'Sunglasses',
+      description: 'Protect your eyes in style with these fashionable sunglasses.',
+      price: 19.99,
+      imageUrl: '',
+    ),
+    Product(
+      id: 'p8',
+      title: 'Running Shoes',
+      description: 'Stay active with these comfortable and supportive running shoes.',
+      price: 79.99,
+      imageUrl: 'https://cdn.pixabay.com/photo/2014/05/18/11/26/shoes-346986_960_720.jpg',
+    ),
+    Product(
+      id: 'p9',
+      title: 'Wristwatch',
+      description: 'Keep track of time in style with this elegant wristwatch.',
+      price: 129.99,
+      imageUrl: '',
+    ),
+    Product(
+      id: 'p10',
+      title: 'Wireless Headphones',
+      description: 'Listen to music or take calls wirelessly with these high-quality headphones.',
+      price: 149.99,
+      imageUrl: '',
+    ),
+    Product(
+      id: 'p11',
+      title: 'Blue Jeans',
+      description: 'A classic pair of blue jeans that will never go out of style.',
+      price: 49.99,
+      imageUrl: '',
+    ),
+    Product(
+      id: 'p12',
+      title: 'Black T-Shirt',
+      description: 'A simple and versatile black t-shirt for everyday wear.',
+      price: 19.99,
+      imageUrl: '',
+    ),
+    Product(
+      id: 'p13',
+      title: 'Leather Wallet',
+      description: 'Keep your cards and cash organized with this stylish leather wallet.',
+      price: 39.99,
+      imageUrl: '',
+    ),
+    Product(
+      id: 'p14',
+      title: 'Hiking Backpack',
+      description: 'Take everything you need for a day hike in this spacious and durable backpack.',
+      price: 69.99,
+      imageUrl: '',
+    )
   ];
 
   List<Product> get items {
