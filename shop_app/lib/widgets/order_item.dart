58875753495f7242b4620fa2107a9e1069d4a1bf
Mose <km_413@naver.com>
2023-05-07 16:41:42 +0900

    Adding an Orders Screen

---
+++ shop_app/lib/widgets/order_item.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../provider/orders.dart' as ord;
+
+class OrderItem extends StatelessWidget {
+  final ord.OrderItem order;
+  const OrderItem(this.order, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text("\$${order.amount.toStringAsFixed(2)}"),
+            subtitle: Text(DateFormat("yMMdd hh:mm:ss").format(order.dateTime)),
+            trailing: IconButton(
+              icon: Icon(Icons.expand_more),
+              onPressed: () {
+
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
