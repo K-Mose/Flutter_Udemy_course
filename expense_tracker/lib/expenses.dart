b69693c1f1d3e2779ba9f679639eeed5da3732ec
Mose <km_413@naver.com>
2023-05-09 17:36:43 +0900

    Creating Dummy Data

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/model/expense.dart';
 import 'package:flutter/material.dart';
 
 
@@ -16,6 +17,80 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: "Hamburger",
+        amount: 9.98,
+        date: DateTime(2023, 4, 15),
+        category: Category.food
+    ),
+    Expense(
+      title: "Airfare",
+      amount: 250.0,
+      date: DateTime(2023, 4, 15),
+      category: Category.travel,
+    ),
+    Expense(
+      title: "Movie Tickets",
+      amount: 25.5,
+      date: DateTime(2023, 4, 20),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: "Office Supplies",
+      amount: 50.75,
+      date: DateTime(2023, 4, 25),
+      category: Category.work,
+    ),
+    Expense(
+      title: "Dinner",
+      amount: 45.99,
+      date: DateTime(2023, 4, 27),
+      category: Category.food,
+    ),
+    Expense(
+      title: "Hotel Accommodation",
+      amount: 150.0,
+      date: DateTime(2023, 4, 30),
+      category: Category.travel,
+    ),
+    Expense(
+      title: "Coffee",
+      amount: 3.5,
+      date: DateTime(2023, 5, 1),
+      category: Category.food,
+    ),
+    Expense(
+      title: "Concert Tickets",
+      amount: 75.0,
+      date: DateTime(2023, 5, 3),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: "Taxi Fare",
+      amount: 20.0,
+      date: DateTime(2023, 5, 5),
+      category: Category.travel,
+    ),
+    Expense(
+      title: "Lunch",
+      amount: 15.99,
+      date: DateTime(2023, 5, 7),
+      category: Category.food,
+    ),
+    Expense(
+      title: "Conference Registration",
+      amount: 200.0,
+      date: DateTime(2023, 5, 10),
+      category: Category.work,
+    ),
+    Expense(
+      title: "Gym Membership",
+      amount: 50.0,
+      date: DateTime(2023, 5, 12),
+      category: Category.leisure,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
