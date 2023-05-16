5d2b5bce82ae3d32803b7f494e6a485a111e1636
Mose <km_413@naver.com>
2023-05-16 16:59:13 +0900

    TextTheme

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -17,7 +17,22 @@ void main() {
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.primaryContainer,
           centerTitle: true
-        )
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer
+          )
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.secondaryContainer,
+            fontSize: 22) // appBar title
+          )
       ),
       home: const Expenses(),
     )
