186568b0a9eae6f72cb5d27ca8ffa2a93568902b
Mose <km_413@naver.com>
2023-05-16 16:31:42 +0900

    ColorSchema

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -2,11 +2,23 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme //
+    .fromSeed(seedColor: const Color.fromARGB(255, 123, 55, 1));
+
 void main() {
   runApp(
     MaterialApp(
       // material3 사용
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        // scaffoldBackgroundColor: Colors.blueGrey,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+          centerTitle: true
+        )
+      ),
       home: const Expenses(),
     )
   );
