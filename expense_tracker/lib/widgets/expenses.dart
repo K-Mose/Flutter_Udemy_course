87076a9b3477e80512499aa8a40cc9e17c25403a
Mose <km_413@naver.com>
2023-05-17 10:59:31 +0900

    Adding Chart Widget

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/model/expense.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
@@ -155,7 +156,7 @@ class _ExpensesState extends State<Expenses> {
         ),
         body: Column(
           children: [
-            const Text("The chart"),
+            Chart(expenses: _registeredExpense),
             Expanded(child: mainContent)
           ],
         ),
