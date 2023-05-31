4bad3549c99c3188077ecc0ab70b8168e6156493
Mose <km_413@naver.com>
2023-05-31 13:18:28 +0900

    new meal app - Tap With InkWell

--- new_meal_ap/lib/widgets/category_grid_item.dart
+++ new_meal_ap/lib/widgets/category_grid_item.dart
@@ -8,22 +8,31 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-            colors: [
-              category.color.withOpacity(0.55),
-              category.color.withOpacity(0.9)
-            ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
-        )
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-          color: Theme.of(context).colorScheme.onBackground,
+    // Gesture Detector Widget
+    return InkWell(
+      onTap: () {
+
+      },
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          gradient: LinearGradient(
+              colors: [
+                category.color.withOpacity(0.7),
+                category.color.withOpacity(0.95)
+              ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight
+          )
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onBackground,
+          ),
         ),
       ),
     );
