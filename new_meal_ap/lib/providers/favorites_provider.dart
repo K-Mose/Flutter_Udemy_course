6843eb4264b97a31b1e74abd4005f6cf818ce1d8
Mose <km_413@naver.com>
2023-06-08 10:26:20 +0900

    new meal app - Creating  a More Complex Provider with StateNotifier

---
+++ new_meal_ap/lib/providers/favorites_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:new_meal_ap/data/dummy_data.dart';
+import 'package:new_meal_ap/model/meal.dart';
+
+// 데이터 변화에 최적화된 Provider (상태관리)
+final favoriteMealsProvider =
+  StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>(
+    (ref) => FavoriteMealsNotifier()
+  );
+
+//StateNotifierProvider는 StateNotifier를 상속받는 클래스와 같이 사용함
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  // initial value
+  FavoriteMealsNotifier(): super([]); // notifier에 저장할 초기 데이터 super에 전달
+
+  void toggleMealFavoriteStatus(Meal meal) {
+    // 상태는 불변값이므로 상태에 대하여 add나 remove등을 할 수 없다.
+    // -> 상태에 값을 재할당
+    if(!state.contains(meal)) {
+      state = [...state, meal];
+    }  else {
+      state = state.where((m) => m.id != meal.id).toList();
+    }
+  }
+}
