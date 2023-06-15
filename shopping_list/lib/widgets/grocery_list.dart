fb037e63681af58b517bd8081aacca9397343dc5
Mose <km_413@naver.com>
2023-06-15 17:06:14 +0900

    shopping list app with Firebase - Request Handling With `FutureBuilder` Widget

--- shopping_list/lib/widgets/grocery_list.dart
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -21,60 +21,39 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   final List<GroceryItem> _groceryItems = [];
-  var _isLoading = true;
-  var _isError = false;
-  var errorMessage = COMMONERRORMESSAGE;
+  late Future<List<GroceryItem>> _loadedItems;
+  var message = COMMONERRORMESSAGE;
   @override
   void initState() {
     super.initState();
-    _loadItems();
+    _loadedItems = _loadItems();
   }
 
-  void _loadItems() async {
-    final url = Uri.https(BASE_URL+".com", "shopping-list.json");
-    try {
-      final response = await http.get(url);
-      final data = json.decode(response.body); // or response.body == "null"
-      print(response.statusCode);
-      print(response.body); // 응답 데이터 모양 체크
-      if (data == null) {
-        setState(() {
-          _groceryItems.clear();
-          _isLoading = false;
-        });
-        return;
-      }
-      if (response.statusCode == 200) {
-        final Map<String, dynamic> listMap = json.decode(response.body);
-        final List<GroceryItem> _loadedItems = [];
-        for (final item in listMap.entries) {
-          _loadedItems.add(GroceryItem(
-              id: item.key,
-              name: item.value["name"],
-              quantity: item.value["quantity"],
-              category: categories.entries.firstWhere((e) => e.value.name == item.value["category"]).value
-          ));
-        }
-        setState(() {
-          _groceryItems.clear();
-          _groceryItems.addAll(_loadedItems);
-          _isLoading = false;
-        });
-      } else if (response.statusCode >= 400) {
-        setState(() {
-          _isLoading = false;
-          _isError = true;
-          errorMessage = ERRORMESSAGE404;
-        });
-      }
-    } catch (err) {
-      print(err);
-      setState(() {
-        _isLoading = false;
-        _isError = true;
-        errorMessage = COMMONERRORMESSAGE;
-      });
+  Future<List<GroceryItem>> _loadItems() async {
+    final url = Uri.https(BASE_URL, "shopping-list.json");
+    final response = await http.get(url);
+    if (response.body == "null") {
+      // snapshot.data!.isEmpty
+      return [];
     }
+
+    if (response.statusCode >= 400) {
+      // snapshot.hasError
+      throw Exception(ERRORMESSAGE404);
+    }
+
+    final Map<String, dynamic> listMap = json.decode(response.body);
+    final List<GroceryItem> _loadedItems = [];
+    for (final item in listMap.entries) {
+      _loadedItems.add(GroceryItem(
+          id: item.key,
+          name: item.value["name"],
+          quantity: item.value["quantity"],
+          category: categories.entries.firstWhere((e) => e.value.name == item.value["category"]).value
+      ));
+    }
+    // snapshot.data!.isNotEmpty
+    return _loadedItems;
   }
 
   void _addItem() async {
@@ -86,6 +65,7 @@ class _GroceryListState extends State<GroceryList> {
         _groceryItems.add(newItem!);
       });
     }
+    _loadedItems = _loadItems();
     // _loadItems();
   }
 
@@ -114,6 +94,7 @@ class _GroceryListState extends State<GroceryList> {
     setState(() {
       _groceryItems.remove(item);
     });
+    _loadedItems = _loadItems();
   }
 
   @override
@@ -128,29 +109,56 @@ class _GroceryListState extends State<GroceryList> {
           )
         ],
       ),
-      body: (_isLoading) ? const Center(child: CircularProgressIndicator(),) :
-      (_isError) ? MessageScreen(message: errorMessage)  :
-      (_groceryItems.isNotEmpty) ? ListView.builder(
-        itemCount: _groceryItems.length,
-        // SwipeToDelete를 위한 Dismissible
-        itemBuilder: (context, index) => Dismissible(
-          key:  UniqueKey(),// ValueKey(_groceryItems[index]),
-          direction: DismissDirection.endToStart,
-          background: Container(color: Colors.red,),
-          onDismissed: (direction) {
-            _removeItem(_groceryItems[index]);
-          },
-          child: ListTile(
-            leading: Container(
-              width: 24,
-              height: 24,
-              color: _groceryItems[index].category.color,
-            ),
-            title: Text(_groceryItems[index].name),
-            trailing: Text("${_groceryItems[index].quantity}"),
-          ),
-        ),
-      ) : const MessageScreen(message: EMPTYMESSAGE),
+      // HTTP response가 Future로 오기 떄문에 처리할 수 있는 FutureBuilder
+      body: FutureBuilder(
+        // future tpye으로 감지할 데이터
+        future: _loadedItems,
+        // future가 데이터를 생성한 뒤에 보여질 화면
+        // snapshot: 현재 state에 접근하여
+        builder: (context, snapshot) {
+          // Future의 current state의 따른 widget 표시
+          // state가 waiting 일 때
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator(),);
+          }
+          // future에서 에러가 발생했을 때
+          if (snapshot.hasError) {
+            // return MessageScreen(message: message);
+            return MessageScreen(message: snapshot.error.toString());
+          }
+          // 데이터가 있을 때
+          if (snapshot.hasData) {
+            // 빈 데이터일 때
+            if (snapshot.data!.isEmpty) {
+              return const MessageScreen(message: EMPTYMESSAGE);
+            }
+            if (snapshot.data!.isNotEmpty) {
+              return ListView.builder(
+                itemCount: _groceryItems.length,
+                // SwipeToDelete를 위한 Dismissible
+                itemBuilder: (context, index) => Dismissible(
+                  key:  UniqueKey(),// ValueKey(_groceryItems[index]),
+                  direction: DismissDirection.endToStart,
+                  background: Container(color: Colors.red,),
+                  onDismissed: (direction) {
+                    _removeItem(_groceryItems[index]);
+                  },
+                  child: ListTile(
+                    leading: Container(
+                      width: 24,
+                      height: 24,
+                      color: _groceryItems[index].category.color,
+                    ),
+                    title: Text(_groceryItems[index].name),
+                    trailing: Text("${_groceryItems[index].quantity}"),
+                  ),
+                ),
+              );
+            }
+          }
+          return const MessageScreen(message: EMPTYMESSAGE);
+        },
+      ),
     );
   }
 }
