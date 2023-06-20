5b06c3f1d4d8c14037e46b01f4eec41b6c469e26
Mose <km_413@naver.com>
2023-06-20 14:57:48 +0900

    favorite places app - Setup SQLite(Sqflite) & Create Database(& Table) & Insert Data

--- favorite_places_app/lib/model/place.dart
+++ favorite_places_app/lib/model/place.dart
@@ -23,7 +23,12 @@ class PlaceLocation {
 }
 
 class Place {
-  Place({required this.title, required this.image, required this.location}) : id = uuid.v4();
+  Place({
+    required this.title,
+    required this.image,
+    required this.location,
+    String? id // id 값이 null 일 때만 uuid로 생성
+  }) : id = id ?? uuid.v4();
 
   final String id;
   final String title;
--- favorite_places_app/lib/providers/place_provider.dart
+++ favorite_places_app/lib/providers/place_provider.dart
@@ -5,9 +5,37 @@ import 'package:favorite_places_app/model/place.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as p;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 final  placeProvider = StateNotifierProvider<_PlaceNotifier, List<Place>>(
         (ref) => _PlaceNotifier());
+
+Future<Database> _getDatabase() async {
+  // DB 생성 후 데이터 삽입
+  final dbPath = await sql.getDatabasesPath();
+  // option : https://pub.dev/documentation/sqflite_common/latest/sqflite/openDatabase.html
+  final db = await sql.openDatabase(
+    // db path
+      p.join(dbPath, 'places.db'),
+      // db가 생성될 때 실행할 함수
+      // 기존에 디비가 존재하지 않을 때 실행됨
+      onCreate: (db, version) {
+        return db.execute(
+            "CREATE TABLE place("
+                "id TEXT PRIMARY KEY, "
+                "title TEXT, "
+                "image TEXT,"
+                "lat REAL,"
+                "long REAL,"
+                "address TEXT"
+                ")"
+        );
+      },
+      version: 1
+  );
+  return db;
+}
 
 class _PlaceNotifier extends StateNotifier<List<Place>> {
   _PlaceNotifier() : super([]);
@@ -20,12 +48,23 @@ class _PlaceNotifier extends StateNotifier<List<Place>> {
     final filename = p.basename(image.path);
     final copiedImage = await image.copy('${appDir.path}/$filename');
 
-
     final place = Place(
         title: title,
         image: copiedImage,
         location: location
     );
+
+    final db = await _getDatabase();
+
+    db.insert("place", {
+      'id': place.id,
+      'title': place.title,
+      'image': place.image.path,
+      'lat': place.location.latitude,
+      'long': place.location.longitude,
+      'address': place.location.address
+    });
+
     state = [place, ...state];
   }
 
