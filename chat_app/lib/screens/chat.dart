53c093da209391a00d374b560c21a34e4d944c94
Mose <km_413@naver.com>
2023-07-12 17:51:17 +0900

    chat app - Push Notification Setup & Get FCM Token
    - 토큰 저장하는 로직이 필요함

--- chat_app/lib/screens/chat.dart
+++ chat_app/lib/screens/chat.dart
@@ -1,10 +1,33 @@
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  // 메시지가 전송될 때 push notification 활성
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    // 사용자에거 알림 권한 요. 필수
+    await fcm.requestPermission();
+    // 기기에 할당되는 토큰,
+    // BE 서버 디비에 저장하여 알림을 받을 수 있도록 함
+    final token = await fcm.getToken();
+
+    print("TOKEN $token}");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
